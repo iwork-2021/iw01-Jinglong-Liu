f265978e8f16a49a373db2527f1e2a22a7f0345c
mac <mac@macdeMacBook-Pro.local>
2021-10-05 14:03:05 +0800

    init ui

---
+++ 191220066/MyCalculator/MyCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by mac on 2021/10/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
